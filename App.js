c471f19ad9d63da19825e66dbded437731efa462
burhancacan <burhan@dusundumde.com>
2022-09-08 15:45:09 +0300

    main

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { useState } from 'react';
+import { Text, TouchableOpacity, Button } from 'react-native';
+import { HStack, VStack, Flex } from 'react-native-flex-layout';
+import checkWinner from './checkWinner';
+function Box({ value, onPress, disabled, highlighted }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <TouchableOpacity disabled={disabled} onPress={onPress}>
+      <Flex w={100} h={100} center style={{ backgroundColor: highlighted ? "lightgreen" : "lightgray" }}>
+        <Text style={{ fontSize: 56 }}>{value}</Text>
+      </Flex>
+    </TouchableOpacity>)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [highlighted, setHighlighted] = useState([]);
+
+  const [winner, setWinner] = useState(null);
+
+  const handlePress = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    const winnerLine = checkWinner(newBoard);
+    if (winnerLine) {
+      setHighlighted(winnerLine);
+      setWinner(currentPlayer);
+      alert(`${currentPlayer} won!`);
+    }
+    else { setCurrentPlayer(prev => prev === "X" ? "O" : "X"); }
+
+  };
+
+  const handleReset = () => {
+    setCurrentPlayer("X");
+    setBoard(Array(9).fill(null));
+    setHighlighted([]);
+    setWinner(null);
+
+  };
+
+  const getBox = (index) => (
+    <Box value={board[index]}
+      onPress={() => handlePress(index)}
+      highlighted={highlighted.includes(index)}
+      disabled={winner || board[index]} />
+
+  );
+
+  return (
+    <VStack fill center spacing={4}>
+      <Text style={{ fontSize: 36 }}> {currentPlayer} to Play  </Text>
+      <HStack spacing={4} shouldWrapChildren>
+        {getBox(0)}
+        {getBox(1)}
+        {getBox(2)}
+
+      </HStack>
+      <HStack spacing={4} shouldWrapChildren>
+        {getBox(3)}
+        {getBox(4)}
+        {getBox(5)}
+      </HStack>
+      <HStack spacing={4} shouldWrapChildren>
+        {getBox(6)}
+        {getBox(7)}
+        {getBox(8)}
+      </HStack>
+      <Button title="Reset" onPress={handleReset} />
+    </VStack>
+  )
+
+}
+
+export default App
